17813ca283cf806562a3c79258eee18c924780a7
Marcus Hellberg <marcus@vaadin.com>
2018-11-14 11:35:47 -0800

    use babel for transpiling

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const webpack = require('webpack');
-const { join } = require('path');
+const { resolve } = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpackMerge = require('webpack-merge');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
@@ -8,21 +8,79 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const modeConfig = env => require(`./build-utils/webpack.${env.mode}.js`)(env);
 const loadPresets = require('./build-utils/loadPresets');
 
+const webcomponentsjs = './node_modules/@webcomponents/webcomponentsjs';
+
+const polyfills = [
+  {
+    from: resolve(`${webcomponentsjs}/webcomponents-*.{js,map}`),
+    to: 'vendor',
+    flatten: true
+  },
+  {
+    from: resolve(`${webcomponentsjs}/bundles/*.{js,map}`),
+    to: 'vendor/bundles',
+    flatten: true
+  },
+  {
+    from: resolve(`${webcomponentsjs}/custom-elements-es5-adapter.js`),
+    to: 'vendor',
+    flatten: true
+  }
+];
+
+const assets = [
+  {
+    from: 'src/img',
+    to: 'img/'
+  }
+];
+
+const plugins = [
+  new CleanWebpackPlugin(['dist']),
+  new webpack.ProgressPlugin(),
+  new HtmlWebpackPlugin({
+    filename: 'index.html',
+    template: './src/index.html',
+    minify: {
+      collapseWhitespace: true,
+      minifyCSS: true,
+      minifyJS: true
+    }
+  }),
+  new CopyWebpackPlugin([...polyfills, ...assets], {
+    ignore: ['.DS_Store']
+  })
+];
+
 module.exports = ({ mode, presets }) => {
   return webpackMerge(
     {
       mode,
-      plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new webpack.ProgressPlugin(),
-        new HtmlWebpackPlugin({
-          filename: 'index.html',
-          template: './src/index.html'
-        }),
-        new CopyWebpackPlugin([{ from: 'src/img', to: 'img/' }], {
-          ignore: ['.DS_Store']
-        })
-      ]
+      output: {
+        filename: '[name].[chunkhash:8].js'
+      },
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            options: {
+              plugins: ['@babel/plugin-syntax-dynamic-import'],
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    useBuiltIns: 'usage',
+                    targets: '>1%, not dead, not ie 11'
+                  }
+                ]
+              ]
+            }
+          }
+        ]
+      },
+      plugins
     },
     modeConfig({ mode, presets }),
     loadPresets({ mode, presets })
